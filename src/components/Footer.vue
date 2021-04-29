ec65c892e92b703e93f28e833835bbe29c756b9c
Ma Meng <develop08@wuzheng.com.cn>
2021-04-29 15:46:03 +0800

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="footer">
         <a style="color:#fff; vertical-align:middle;" href="http://marcoma.site/">marcoma.site</a>
-        <img style="margin-left: 10px; width:20px; vertical-align:middle;" src="../assets/image/beian.png">
-        <a style="color:#fff; margin-left: 10px; vertical-align:middle;" target="_blank" rel="noopener" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=2021015312">鲁ICP备2021015312号</a>
+        <span class="separator"></span>
+        <a style="color:#fff; vertical-align:middle;" target="_blank" rel="noopener" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=2021015312">鲁ICP备2021015312号</a>
     </div>
 </template>
 
@@ -19,5 +19,14 @@ export default {
     width: 100%;
     padding: 10px 0;
     background-color: rgb(59, 60, 61);
+}
+
+.separator {
+	border-right: 1px solid #efefef;
+	border-left: 1px solid #efefef;
+	margin: 0px 10px;
+	height: 80px;
+	font-size: inherit;
+    vertical-align:middle;
 }
 </style>
