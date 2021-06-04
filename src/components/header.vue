61b2a632c297abeac843c7988208b8e41a5933ab
Ma Meng <develop08@wuzheng.com.cn>
2021-06-04 12:48:00 +0800

    for Mr.Sun

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,5 @@
 <template>
-    <p class="header">热烈祝贺孙福飞同学获得国家公派留学资格！！！</p>
+    <p class="header">热烈祝贺孙福飞获得公派留学资格！</p>
 </template>
 
 <script>
@@ -15,7 +15,7 @@ export default {
     line-height: 70px;
     color: #ffffff;
     background-color: #cc0000;
-    font-size: 24px;
+    font-size: 20px;
     border-bottom: 1px solid #ffffff;
 }
 </style>
